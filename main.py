4fb92a7fa42f9a378498cfeaac172f22cdf2898e
arraisa <alvaroramo23@gmail.com>
2022-07-12 20:57:19 +0200

    Añadido flujo completo en main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from data_treatment import ArtistsCatalogueTreat
 from data_treatment import MomaTreat
 from data_treatment import TateModernTreat
 from data_treatment import RijksmuseumTreat
-from data_treatment import ArtistsCatalogueTreat
 from utils.Treatment import GetInfoArtObjects
 import pandas as pd
 import matplotlib
@@ -11,19 +11,37 @@ if __name__ == '__main__':
     pd.options.display.max_rows = None
     pd.options.display.max_columns = None
 
-    # df_raw = RijksmuseumTreat.RijksmuseumTreat()
-    # df_raw.silver_to_gold()
+    # CATÁLOGO DE ARTISTAS
+    print("Generamos catálogo de artistas")
+    artists_catalogue = ArtistsCatalogueTreat.ArtistCatalogueTreat()
+    print("Catálogo de artistas de Bronze a Silver")
+    artists_catalogue.bronze_to_silver()
+    print("Catálogo de artistas de Silver a Gold")
+    artists_catalogue.silver_to_gold()
 
-    # df_artists_catalogue = ArtistsCatalogueTreat.ArtistCatalogueTreat()
-    # df_artists_catalogue.silver_to_gold()
+    # MOMA
+    moma_artists = MomaTreat.ArtistTreat()
+    moma_artists.bronze_to_silver()
+    moma_artworks = MomaTreat.ArtworksTreat()
+    moma_artworks.bronze_to_silver()
 
-    # df_moma = MomaTreat.SilverToGold()
-    # df_moma.silver_to_gold()
-    #
-    # #
-    # #
-    # df_moma = MomaTreat.SilverToGold()
-    # df = df_moma.silver_to_gold()
+    print("Catálogo de MOMA de Silver a Gold")
+    moma_silver_to_gold = MomaTreat.SilverToGold()
+    moma_silver_to_gold.silver_to_gold()
 
-    with_catalogue = TateModernTreat.TateTreat()
-    with_catalogue.silver_to_gold()
+    # TATE
+    tate = TateModernTreat.TateTreat()
+    print("Catálogo de TATE de Bronze a Silver")
+    tate.bronze_to_silver()
+    print("Catálogo de TATE de Silver a Gold")
+    tate.silver_to_gold()
+
+
+    # RIJKS
+    rijks = RijksmuseumTreat.RijksmuseumTreat()
+    print("Catálogo de RIJKSMUSEUM de Bronze a Silver")
+    rijks.bronze_to_silver()
+    print("Catálogo de RIJKSMUSEUM de Silver a Gold")
+    rijks.silver_to_gold()
+
+
